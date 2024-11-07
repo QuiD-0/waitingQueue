b94e515aa4d257e370eeadaa50d9b0e2d3d99fa3
quid-0 <wodnd101@naver.com>
2024-11-07 16:20:13 +0900

    대기열 추가여부 테스트

---
+++ entry/src/test/kotlin/com/quid/entry/execute/domain/TicketServiceTest.kt
@@ -0,0 +1,50 @@
+package com.quid.entry.execute.domain
+
+import com.quid.entry.execute.infra.repository.TicketRepository
+import com.quid.entry.execute.infra.repository.WaitingQueueRepository
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito.mock
+import java.time.LocalDateTime
+
+class TicketServiceTest {
+
+    private val waitingQueueRepository = mock<WaitingQueueRepository>()
+    private val ticketRepository = mock<TicketRepository>()
+    private val ticketService = TicketService(10, waitingQueueRepository, ticketRepository)
+
+    @Test
+    @DisplayName("이미 대기열에 등록된경우 대기 필요")
+    fun needWaiting1() {
+        val ticket = Ticket("http://localhost:8080", 1, LocalDateTime.now())
+
+        given(waitingQueueRepository.existsBy(ticket.redirectUrl, ticket.memberSeq)).willReturn(true)
+
+        assertTrue(ticketService.needWaiting(ticket))
+    }
+
+    @Test
+    @DisplayName("대기열이 활성화된 경우 대기 필요")
+    fun needWaiting2() {
+        val ticket = Ticket("http://localhost:8080", 1, LocalDateTime.now())
+
+        given(waitingQueueRepository.existsBy(ticket.redirectUrl, ticket.memberSeq)).willReturn(false)
+        given(waitingQueueRepository.getWaitingCount(ticket.redirectUrl)).willReturn(10)
+
+        assertTrue(ticketService.needWaiting(ticket))
+    }
+
+    @Test
+    @DisplayName("서버 입계값이 제한을 초과한 경우 대기 필요")
+    fun needWaiting3() {
+        val ticket = Ticket("http://localhost:8080", 1, LocalDateTime.now())
+
+        given(waitingQueueRepository.existsBy(ticket.redirectUrl, ticket.memberSeq)).willReturn(false)
+        given(waitingQueueRepository.getWaitingCount(ticket.redirectUrl)).willReturn(0)
+        given(waitingQueueRepository.getActiveCount(ticket.redirectUrl)).willReturn(11)
+
+        assertTrue(ticketService.needWaiting(ticket))
+    }
+}
